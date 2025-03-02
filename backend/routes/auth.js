6317eb4f65851c13f8539f984791bf30ac289212
Ankit <ak398429@gmail.com>
2025-03-02 16:29:21 +0530

    `Added JWT authentication for user login and registration`

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body , validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Ankitisagood$boy';
 
 router.post('/',[
     body('name','Enter a valid name').isLength({ min: 3 }),
@@ -20,20 +24,62 @@ router.post('/',[
         if(user){
             return res.status(400).json({error: "Sorry a user with this email already exists"});
     }
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
 
     user = await User.create(
         {
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
         }
     );
 
-    res.json(user);
-} catch (error) {
+    // res.json(user);
+
+const data = {
+    user: {
+        id: user.id
+    }}
+    const authToken = jwt.sign(data, JWT_SECRET);
+    res.json(authToken);
+}
+ catch (error) {
     console.error(error.message);
     res.status(500).send("Some error occured");
 }
 });
+router.post('/login',[
+    
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password cannot be blank').exists()
+],async(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email, password} = req.body;
+    try{
+        let user = await User.findOne({email});
+            if(!user){
+                return res.status(400).json({error: "Please try to login with correct credentials"});
+            }
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if(!passwordCompare){
+                return res.status(400).json({error: "Please try to login with correct credentials"});
+            }
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET);
+            res.json(authToken);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+});
+
 
 module.exports = router;
